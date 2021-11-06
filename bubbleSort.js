96bb68be0d22e647772deccb8d8cb96c0882041e
yangfeng <crazyyoung1020@163.com>
2021-11-06 12:57:41 +0800

    调整冒泡排序

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,13 +7,13 @@
 // 初次通过的代码
 function bubbleSort(arr){
     // let temp
-    for(let i=0;i<arr.length;i++){
-        for(let j=i+1;j<arr.length;j++){
-            if(arr[i]>arr[j]){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
                 // temp = arr[j]
                 // arr[j] = arr[i]
                 // arr[i] = temp
-                [arr[i],arr[j]] = [arr[j],arr[i]]
+                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
             }
         }
     }
